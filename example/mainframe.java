0846a973d8677803c512382308d076362434e104
shoaib Rizwan <shoabi@ytel.co.in>
2016-05-26 09:25:52 +0530

    3 times System.out.println

--- example/mainframe.java
+++ example/mainframe.java
@@ -2,5 +2,7 @@ public class mainframe{
 	//First Commit
 	public static void main(String arg[]){
 		System.out.println("Testing GitHub ");
+		System.out.println("Testing GitHub ");
+		System.out.println("Testing GitHub ");
 	}
 }
