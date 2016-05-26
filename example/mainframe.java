9e907f8b1795af1be50f09064ef3eae591641d07
shoaib Rizwan <shoabi@ytel.co.in>
2016-05-26 09:18:56 +0530

    space added

--- example/mainframe.java
+++ example/mainframe.java
@@ -2,6 +2,6 @@ public class mainframe{
 	//First Commit
 	public static void main(String arg[]){
 		System.out.println("Testing GitHub ");
-		System.out.println("Testing GitHub ");
+		System.out.println("Testing GitHub");
 	}
 }
